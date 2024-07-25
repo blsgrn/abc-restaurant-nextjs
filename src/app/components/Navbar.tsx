206951628752812c52dcba3b3859701ed40bea13
S Segaran <blsgrn@tutanota.com>
2024-07-25 12:50:11 +0530

    make navbar responsive

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -12,7 +12,7 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-center container py-6">
+    <nav className="flex items-center justify-center md:justify-around container p-6 ">
       <div className="flex items-center ">
         <Image
           src={Logo}
@@ -22,7 +22,7 @@ const Navbar = () => {
           alt="Logo"
           className="bg-violet-900 flex p-2"
         />
-        <div className="navbar-center bg-accent p-6 mx-6 rounded-md flex gap-x-24">
+        <div className="navbar-center bg-accent p-6 mx-6 rounded-md flex gap-x-24 2xl:gap-x-12 lg:gap-x-4 md:hidden">
           {navLinks.map((item, index) => (
             <p className="font-medium text-neutral-800" key={index}>
               {item.name}
@@ -30,20 +30,19 @@ const Navbar = () => {
           ))}
         </div>
       </div>
-      <div className="navbar-end rounded-md flex gap-x-8">
-        <p className="font-medium text-neutral-600 my-auto">
-          Create An Account
-        </p>
+      <div className="navbar-end rounded-md flex gap-x-8 md:gap-x-4">
         <div className="flex items-center gap-x-2">
           <FaRegUser color="#4d1d95" size={28} alt="User Profile" />
-          <span className="font-medium text-neutral-600 lg:block">Sign in</span>
+          <span className="font-medium text-neutral-600 sm:hidden">
+            Sign in
+          </span>
         </div>
 
         <CiTextAlignJustify
           size={40}
           color="#4d1d95"
           alt="User Profile"
-          className="lg:hidden"
+          className="hidden md:block"
         />
       </div>
     </nav>
